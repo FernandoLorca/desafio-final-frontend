6cd9990f13003b485fa0b38769385ef3126d7331
FernandoLorca <florcapinto@gmail.com>
2023-07-04 20:58:54 -0400

    Adding logo. Switching to master to install react icons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import ButtonCta from './ButtonCta';
+import Navbar from './Navbar';
 
 const Hero = () => {
   return (
     <div className="repeat relative h-screen bg-[url('/hero.webp')] bg-cover bg-center bg-no-repeat text-dark-100">
+      <div className="absolute z-20">
+        <Navbar />
+      </div>
       <div className="absolute z-10 h-full w-full bg-dark-900 opacity-60"></div>
       <div className="relative z-20 flex h-full flex-col justify-center gap-2 p-5">
         <h1 className="text-6xl font-bold">
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <div>
+      <img
+        src="/logo.svg"
+        alt=""
+        className="w-16"
+      />
+      <div>
+        <img
+          src=""
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
