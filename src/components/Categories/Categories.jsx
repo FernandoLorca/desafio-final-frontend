bf69cc26574a412caf413f3fee3d2e87d21310ac
FernandoLorca <florcapinto@gmail.com>
2023-07-05 11:01:57 -0400

    Add ux to categories items. Transition, slaces, etc

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,37 +2,39 @@ import Categorie from './Categorie';
 
 const Categories = () => {
   return (
-    <div className="px-5 py-10">
-      <h2 className="mb-5 text-4xl font-bold md:px-20">Categorias</h2>
-      <div className="grid grid-cols-2 grid-rows-[10rem_10rem_10rem_10rem] gap-5 text-center md:grid-cols-4 md:grid-rows-[10rem_10rem] md:px-20 lg:flex lg:h-32">
-        <Categorie
-          title="Placas madre"
-          bgConfig="bg-[url('/categories/mother.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Ram"
-          bgConfig="bg-[url('/categories/ram.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Almacenamiento"
-          bgConfig="bg-[url('/categories/ssd.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Gabinetes"
-          bgConfig="bg-[url('/categories/gabinetes.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Fuentes de poder"
-          bgConfig="bg-[url('/categories/fuente.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Tarjetas de video"
-          bgConfig="bg-[url('/categories/video.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Procesadores"
-          bgConfig="bg-[url('/categories/proce.webp')] bg-repeat bg-center bg-cover"
-        />
+    <div className="flex justify-center">
+      <div className="px-5 py-10 lg:w-full lg:max-w-[84.3rem]">
+        <h2 className="mb-5 text-4xl font-bold md:px-20">Categorias</h2>
+        <div className="grid grid-cols-2 grid-rows-[10rem_10rem_10rem_10rem] gap-5 text-center md:grid-cols-4 md:grid-rows-[10rem_10rem] md:px-20 lg:flex lg:h-32">
+          <Categorie
+            title="Placas madre"
+            bgConfig="bg-[url('/categories/mother.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Ram"
+            bgConfig="bg-[url('/categories/ram.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Almacenamiento"
+            bgConfig="bg-[url('/categories/ssd.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Gabinetes"
+            bgConfig="bg-[url('/categories/gabinetes.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Fuentes de poder"
+            bgConfig="bg-[url('/categories/fuente.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Tarjetas de video"
+            bgConfig="bg-[url('/categories/video.webp')] bg-repeat bg-center bg-cover"
+          />
+          <Categorie
+            title="Procesadores"
+            bgConfig="bg-[url('/categories/proce.webp')] bg-repeat bg-center bg-cover"
+          />
+        </div>
       </div>
     </div>
   );
