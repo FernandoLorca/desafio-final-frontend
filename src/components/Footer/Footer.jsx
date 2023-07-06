4de87f714f9e964ad5baee3502bc76b1a8f0b340
FernandoLorca <florcapinto@gmail.com>
2023-07-06 14:16:27 -0400

    footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,10 +4,12 @@ import Logo from '../Logo';
 import TitleTwo from '../Titles.jsx/TitleTwo';
 import Link from '../Buttons/Link';
 import Rrss from '../RRSS';
+import ButtonPassive from '../Buttons/ButtonPassive';
+import ButtonCta from '../Buttons/ButtonCta';
 
 const Footer = () => {
   return (
-    <div className="mt-10 bg-gradient-to-b from-primary-800 to-primary-900 px-5 text-dark-100">
+    <div className="mt-10 flex flex-col items-center bg-gradient-to-b from-primary-800 to-primary-900 px-5 text-dark-100">
       <div className="py-10">
         <Logo
           logoSize="max-w-[4rem]"
@@ -15,67 +17,56 @@ const Footer = () => {
         />
       </div>
       <div className="pb-5 text-center">
-        <TitleTwo
-          title="Perfil"
-          textSize="text-4xl"
-        />
-        <div className="flex flex-col gap-5">
-          <Link
-            text="Entrar"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
+        <div className="flex justify-center gap-5 py-5 md:gap-3">
+          <ButtonPassive text="Entrar" />
+          <ButtonCta
             text="Registrarse"
-            href="#"
-            fontSize="text-lg"
+            textSize="text-sm"
           />
         </div>
       </div>
-      <div className="py-5 text-center">
-        <TitleTwo
-          title="Categorías"
-          textSize="text-4xl"
+      <TitleTwo
+        title="Categorías"
+        textSize="text-4xl text-center"
+      />
+      <div className="flex flex-col gap-5 text-center md:flex-row">
+        <Link
+          text="Placas madre"
+          href="#"
+          fontSize="text-sm"
         />
-        <div className="flex flex-col gap-5">
-          <Link
-            text="Placas madre"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Ram"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Ssd y Hdd"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Gabinetes"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Fuentes de poder"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Tarjetas de video"
-            href="#"
-            fontSize="text-lg"
-          />
-          <Link
-            text="Procesadores"
-            href="#"
-            fontSize="text-lg"
-          />
-        </div>
+        <Link
+          text="Ram"
+          href="#"
+          fontSize="text-sm"
+        />
+        <Link
+          text="Ssd y Hdd"
+          href="#"
+          fontSize="text-sm"
+        />
+        <Link
+          text="Gabinetes"
+          href="#"
+          fontSize="text-sm"
+        />
+        <Link
+          text="Fuentes de poder"
+          href="#"
+          fontSize="text-sm"
+        />
+        <Link
+          text="Tarjetas de video"
+          href="#"
+          fontSize="text-sm"
+        />
+        <Link
+          text="Procesadores"
+          href="#"
+          fontSize="text-sm"
+        />
       </div>
-      <div className="flex justify-center gap-7 py-5">
+      <div className="flex justify-center gap-7 pb-5 pt-10">
         <Rrss icon={<BsInstagram />} />
         <Rrss icon={<BsFacebook />} />
         <Rrss icon={<BsTwitter />} />
