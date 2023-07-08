b72f4c7cccd0ad3cfe87efb69ac52bf2d2c9711c
FernandoLorca <florcapinto@gmail.com>
2023-07-08 17:51:35 -0400

    Form and inputs layout done

--- src/components/Buttons/ButtonCta.jsx
+++ src/components/Buttons/ButtonCta.jsx
@@ -1,7 +1,7 @@
 const ButtonCta = ({ text, textSize }) => {
   return (
     <button
-      className={`max-w-max rounded-full bg-gradient-to-br from-primary-600 to-secondary-700 px-6 py-1 hover:bg-gradient-to-br hover:from-primary-700 hover:to-secondary-600 ${textSize}`}
+      className={`max-w-max rounded-full bg-gradient-to-br from-primary-600 to-secondary-700 px-6 py-1 hover:bg-gradient-to-br hover:from-primary-700 hover:to-secondary-600 focus:outline-none active:bg-primary-500 ${textSize}`}
     >
       {text}
     </button>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,30 @@
 import ButtonCta from '../components/Buttons/ButtonCta';
+import FormInput from '../components/Form/FormInput';
+import Logo from '../components/Logo';
 import TitleOne from '../components/Titles.jsx/TitleOne';
 
 const Login = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-primary-900 bg-[url('/hero.webp')] bg-cover bg-[center_left_-30rem] bg-no-repeat text-dark-800 md:bg-[center_left_-18rem] lg:bg-center">
-      <div className="mx-10 flex w-full flex-col rounded-xl bg-dark-100 p-10">
+    <div className="flex h-screen items-center justify-center bg-primary-900 bg-[url('/hero.webp')] bg-cover bg-center bg-no-repeat text-dark-800">
+      <div className="mx-10 flex w-full flex-col rounded-xl bg-dark-100 p-8 md:max-w-[40rem]">
+        <div className="mb-5">
+          <Logo
+            logoSize="max-w-[3rem] lg:max-w-[3rem]"
+            fontSize="text-xl lg:text-xl text-primary-700"
+          />
+        </div>
         <TitleOne
           title="Ingresa a tu cuenta"
-          classProperty="text-2xl"
+          classProperty="text-xl"
         />
         <form className="flex flex-col gap-5">
-          <input
-            type="text"
-            placeholder="Correo"
+          <FormInput
+            inputType="text"
+            inputPlaceholder="Correo"
           />
-          <input
-            type="password"
-            placeholder="Contraseña"
+          <FormInput
+            inputType="password"
+            inputPlaceholder="Contraseña"
           />
           <ButtonCta
             text="Ingresar"
