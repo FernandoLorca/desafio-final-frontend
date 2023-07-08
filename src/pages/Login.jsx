0550e9809b8b50e179712f02651d60e30bf2886f
FernandoLorca <florcapinto@gmail.com>
2023-07-08 15:02:08 -0400

    Create layout for login page. Now is time to work on forms

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex h-screen items-center justify-center bg-primary-900 bg-[url('/hero.webp')] bg-cover bg-[center_left_-30rem] bg-no-repeat md:bg-[center_left_-18rem] lg:bg-center">
+      <div className="bg-dark-100">
+        <h1>Ingresa a tu cuenta</h1>
+      </div>
     </div>
   );
 };
