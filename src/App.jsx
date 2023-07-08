5f2f68654fa91b6dc6d165f22a6b22cfe30f84aa
FernandoLorca <florcapinto@gmail.com>
2023-07-08 10:56:20 -0400

    Create the config for react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import Hero from './components/Hero/Hero';
-import Categories from './components/Categories/Categories';
-import Products from './components/Products/Products';
-import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+
+import HomePublic from './pages/HomePublic';
 
 const App = () => {
   return (
-    <div className="text-dark-800">
-      <Hero />
-      <Categories />
-      <Products title="Tarjetas de video" />
-      <Products title="Ssd y Hdd" />
-      <Products title="Procesadores" />
-      <Footer />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={<HomePublic />}
+      />
+      <Route
+        path="*"
+        element={<h1>404</h1>}
+      />
+    </Routes>
   );
 };
 
---
+++ src/pages/HomePublic.jsx
@@ -0,0 +1,19 @@
+import Hero from './components/Hero/Hero';
+import Categories from './components/Categories/Categories';
+import Products from './components/Products/Products';
+import Footer from './components/Footer/Footer';
+
+const HomePublic = () => {
+  return (
+    <div className="text-dark-800">
+      <Hero />
+      <Categories />
+      <Products title="Tarjetas de video" />
+      <Products title="Ssd y Hdd" />
+      <Products title="Procesadores" />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePublic;
