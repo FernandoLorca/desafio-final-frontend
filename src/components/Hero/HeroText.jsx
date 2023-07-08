b90fe8ee82987c9a6e3db558893ae3934a5d319d
FernandoLorca <florcapinto@gmail.com>
2023-07-08 15:17:14 -0400

    Change some detail in the TitleOne component that is in the Hero component

--- src/components/Hero/HeroText.jsx
+++ src/components/Hero/HeroText.jsx
@@ -3,7 +3,11 @@ import TitleOne from '../Titles.jsx/TitleOne';
 const HeroText = ({ title, subTitle }) => {
   return (
     <>
-      <TitleOne title={title} /> <p className="text-2xl">{subTitle}</p>
+      <TitleOne
+        title={title}
+        classProperty="text-6xl"
+      />
+      <p className="text-2xl">{subTitle}</p>
     </>
   );
 };
