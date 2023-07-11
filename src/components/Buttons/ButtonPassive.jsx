21a9a0848dee0e3d00f99f95dccedbe1d2d40a9c
FernandoLorca <florcapinto@gmail.com>
2023-07-11 08:04:09 -0400

    change on passive button

--- src/components/Buttons/ButtonPassive.jsx
+++ src/components/Buttons/ButtonPassive.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 const ButtonPassive = ({ text, classProperty, toHref }) => {
   return (
     <Link to={toHref}>
-      <button className={`rounded-full border border-primary-100 px-6 py-3 text-lg hover:border hover:border-primary-600 hover:bg-primary-600 hover:text-secondary-100 lg:py-1 ${classProperty}`}>
+      <button
+        className={`w-full rounded-full border border-primary-100 px-6 py-3 text-lg hover:border hover:border-primary-600 hover:bg-primary-600 hover:text-secondary-100 lg:py-1 ${classProperty}`}
+      >
         {text}
       </button>
     </Link>
