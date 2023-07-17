953a72f63c1323de5c5d0cf7cbc8d72bbeadb280
FernandoLorca <florcapinto@gmail.com>
2023-07-17 15:29:05 -0400

    Refactor categories sections of home page. Map to json that has every categorie

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,5 +1,6 @@
 import TitleTwo from '../Titles.jsx/TitleTwo';
 import Categorie from './Categorie';
+import categories from '../Categories/categories.json';
 
 const Categories = () => {
   return (
@@ -11,34 +12,13 @@ const Categories = () => {
         />
       </div>
       <div className="grid grid-cols-2 grid-rows-[10rem_10rem_10rem_10rem] gap-5 text-center md:grid-cols-4 md:grid-rows-[10rem_10rem] md:px-16 lg:flex lg:h-32 lg:justify-between">
-        <Categorie
-          title="Placas madre"
-          bgConfig="bg-[url('/categories/mother.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Ram"
-          bgConfig="bg-[url('/categories/ram.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Ssd y Hdd"
-          bgConfig="bg-[url('/categories/ssd.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Gabinetes"
-          bgConfig="bg-[url('/categories/gabinetes.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Fuentes de poder"
-          bgConfig="bg-[url('/categories/fuente.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Tarjetas de video"
-          bgConfig="bg-[url('/categories/video.webp')] bg-repeat bg-center bg-cover"
-        />
-        <Categorie
-          title="Procesadores"
-          bgConfig="bg-[url('/categories/proce.webp')] bg-repeat bg-center bg-cover"
-        />
+        {categories.map(categorie => (
+          <Categorie
+            key={categorie.id}
+            title={categorie.name}
+            bgConfig={`bg-[url('${categorie.img}')] bg-repeat bg-center bg-cover`}
+          />
+        ))}
       </div>
     </div>
   );
