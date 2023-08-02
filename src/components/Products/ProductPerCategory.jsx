978be7b7b016e4e362c5b6660b8b1d847e6f83b3
FernandoLorca <florcapinto@gmail.com>
2023-08-02 08:48:33 -0400

    loader animation done

--- src/components/Products/ProductPerCategory.jsx
+++ src/components/Products/ProductPerCategory.jsx
@@ -1,12 +1,13 @@
 import { BsArrowRepeat } from 'react-icons/bs';
 import ProductTitleLink from './ProductTitleLink';
 import ProductPrice from './ProductPrice';
+import './ProductPerCategory.css';
 
 const ProductPerCategory = ({ title, price, img, id, category }) => {
   return (
     <div className="grid h-full content-around justify-items-center">
       <div className="mb-2 flex max-h-[10rem] min-h-[10rem] w-full max-w-[10rem] items-center justify-center rounded-xl border-2 border-dark-100 p-6">
-        <div className="text-4xl text-primary-800">
+        <div className="animation text-4xl text-primary-800">
           <BsArrowRepeat />
         </div>
         {/* <img
